9eb3794d9b350ce72cc718b76016b00c4d2cacd3
mramydnei <mramydnei@gmail.com>
2023-10-28 23:15:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -18,49 +18,49 @@ function create_csv()
     }
 
     let newBtn = document.createElement('button');
-newBtn.innerHTML = "CSVダウンロード";
-newBtn.id = "download";
-document.getElementsByClassName("list-tab")[0].appendChild(newBtn);
+    newBtn.innerHTML = "CSVダウンロード";
+    newBtn.id = "download";
+    newBtn.setAttribute("style", "width:250px;height:30px");
+    document.getElementsByClassName("list-tab")[0].appendChild(newBtn);
 
 
-function downloadCSV() {
-    //ダウンロードするCSVファイル名を指定する
-    const filename = "download.csv";
-    //CSVデータ
-    const data = csv_data;
-    //BOMを付与する（Excelでの文字化け対策）
-    const bom = new Uint8Array([0xef, 0xbb, 0xbf]);
-    //Blobでデータを作成する
-    const blob = new Blob([bom, data], {
-        type: "text/csv"
-    });
+    function downloadCSV() {
+        //ダウンロードするCSVファイル名を指定する
+        const filename = "download.csv";
+        //CSVデータ
+        const data = csv_data;
+        //BOMを付与する（Excelでの文字化け対策）
+        const bom = new Uint8Array([0xef, 0xbb, 0xbf]);
+        //Blobでデータを作成する
+        const blob = new Blob([bom, data], {
+            type: "text/csv"
+        });
 
 
-    //IE10/11用(download属性が機能しないためmsSaveBlobを使用）
-    if (window.navigator.msSaveBlob) {
-        window.navigator.msSaveBlob(blob, filename);
+        //IE10/11用(download属性が機能しないためmsSaveBlobを使用）
+        if (window.navigator.msSaveBlob) {
+            window.navigator.msSaveBlob(blob, filename);
 
-        //その他ブラウザ
-    } else {
-        //BlobからオブジェクトURLを作成する
-        const url = (window.URL || window.webkitURL).createObjectURL(blob);
-        //ダウンロード用にリンクを作成する
-        const download = document.createElement("a");
-        //リンク先に上記で生成したURLを指定する
-        download.href = url;
-        //download属性にファイル名を指定する
-        download.download = filename;
-        //作成したリンクをクリックしてダウンロードを実行する
-        download.click();
-        //createObjectURLで作成したオブジェクトURLを開放する
-        (window.URL || window.webkitURL).revokeObjectURL(url);
+            //その他ブラウザ
+        } else {
+            //BlobからオブジェクトURLを作成する
+            const url = (window.URL || window.webkitURL).createObjectURL(blob);
+            //ダウンロード用にリンクを作成する
+            const download = document.createElement("a");
+            //リンク先に上記で生成したURLを指定する
+            download.href = url;
+            //download属性にファイル名を指定する
+            download.download = filename;
+            //作成したリンクをクリックしてダウンロードを実行する
+            download.click();
+            //createObjectURLで作成したオブジェクトURLを開放する
+            (window.URL || window.webkitURL).revokeObjectURL(url);
+        }
     }
+
+    //ボタンを取得する
+    const download = document.getElementById("download");
+    //ボタンがクリックされたら「downloadCSV」を実行する
+    download.addEventListener("click", downloadCSV, false);
+
 }
-
-//ボタンを取得する
-const download = document.getElementById("download");
-//ボタンがクリックされたら「downloadCSV」を実行する
-download.addEventListener("click", downloadCSV, false);
-
-}
-
